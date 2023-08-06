2e38d839a5ca4e6d7d8dd0efe464fd21706d1190
blackwinggoat <46078819+blackwinggoat@users.noreply.github.com>
2023-08-06 21:16:29 +0800

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,20 +1,27 @@
 import openai
 import streamlit as st
 
-openai_api_key = "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"
+# Please replace 'your-api-key' with your actual OpenAI API key
+openai.api_key = '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'
 
-st.title("🐈 Wiyata Tutor AI XXX")
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+st.title("OpenAI Chatbot")
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
 
-if prompt := st.chat_input():
-    openai.api_key = openai_api_key
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
-    st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+if st.session_state["chat_history"]:
+    for message in st.session_state["chat_history"]:
+        if message["role"] == "system" or message["role"] == "assistant":
+            st.write(f'AI: {message["content"]}')
+        else:
+            st.write(f'User: {message["content"]}')
+
+user_input = st.text_input("Your Message:")
+if st.button("Send"):
+    if user_input:
+        st.session_state["chat_history"].append({"role": "user", "content": user_input})
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=st.session_state["chat_history"]
+        )
+        st.session_state["chat_history"].append(response.choices[0].message)
